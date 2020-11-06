becff6cea9e4b05e15b276079cc4757c7fd34c1c
Megh Parikh <meghprkh@gmail.com>
2020-11-07 01:11:48 +0530

    error_handling and option

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,15 +4,19 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` for hints!
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
+use std::error::Error;
 
-fn main() {
+// M1:
+// fn main() {
+// M2:
+fn main() -> Result<(), Box<dyn Error>> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    // M1: let cost = total_cost(pretend_user_input).unwrap();
+    // M2:
+    let cost = total_cost(pretend_user_input).unwrap();
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -20,6 +24,9 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+
+    // M2:
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
